cc04aaf837178134e13e83ecdd0ff7b2eb14f607
Starfall Production <starfallproduction42@gmail.com>
2019-07-02 02:18:57 +0700

    Implement fully working Dialog to Project

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,9 +1,103 @@
 import React, {useState, useEffect} from 'react';
-import {useSelector} from 'react-redux';
+import {useSelector, useDispatch} from 'react-redux';
 import MaterialTable from 'material-table';
 import {Search, Clear, ArrowForward, ArrowBack, FirstPage, LastPage, DeleteForever, Create, Add} from '@material-ui/icons'
 import {CREATOR, MANAGER} from '../actions/authorizationActions';
 import {Link} from 'react-router-dom';
+
+import { ADD_DIALOG, DELETE_DIALOG, RENAME_DIALOG, HideDialog
+    , ShowAddDialog, ShowDeleteDialog, ShowRenameDialog
+    , ShowNotificationDialog, HideNotificationDialog, SetTarget} from '../actions/globalActions';
+import DialogRename from './DialogRename';
+import DialogDelete from './DialogDelete';
+import DialogAdd from './DialogAdd';
+import DialogNotification from './DialogNotification';
+
+function Authorized(props){
+    const dispatch = useDispatch();
+    const target = useSelector(state => state.target);
+    const dialogType = useSelector(state=>state.dialog);
+    const notification = useSelector(state=>state.notification);
+
+    return(
+        <React.Fragment>
+            <DialogAdd
+                dialogProps={{
+                    open: dialogType === ADD_DIALOG,
+                    onClose: () => dispatch(HideDialog())
+                }}
+                addCategory="Entity"
+                addRequest={`entity/${target.id}`}
+                onSucceed={(res, name) => {
+                    dispatch(HideDialog());
+                    dispatch(ShowNotificationDialog(
+                        `Add New Entity ${name}`, 
+                        `Succeed adding new entity ${name}`
+                    ));
+                }}
+                onFail={(err, name) => { 
+                    dispatch(HideDialog());
+                    dispatch(ShowNotificationDialog(
+                        `Add New Entity ${name}`, 
+                        `Fail adding new entity ${name}, error: ${err}`
+                    ));
+                }}
+            />
+            <DialogDelete
+                dialogProps={{
+                    open: dialogType === DELETE_DIALOG,
+                    onClose: () => dispatch(HideDialog())
+                }}
+                targetName={target.name}
+                deleteRequest={`entity/${target.id}`}
+                onSucceed={(res, name) => {
+                    dispatch(HideDialog());
+                    dispatch(ShowNotificationDialog(
+                        `Delete Entity ${name}`, 
+                        `Succeed deleting entity ${name}`
+                    ));
+                }}
+                onFail={(err, name) => { 
+                    dispatch(HideDialog());
+                    dispatch(ShowNotificationDialog(
+                        `Delete Entity ${name}`, 
+                        `Fail deleting entity ${name}, error: ${err}`
+                    ));
+                }}
+            />
+            <DialogRename
+                dialogProps={{
+                    open: dialogType === RENAME_DIALOG,
+                    onClose: () => dispatch(HideDialog())
+                }}
+                renameRequest={`entity/${target.id}/rename`}
+                targetName={target.name}
+                onSucceed={(res, name, newName) => {
+                    dispatch(HideDialog());
+                    dispatch(ShowNotificationDialog(
+                        `Rename Entity ${name}`, 
+                        `Succeed renaming entity ${name} into ${newName}`
+                    ));
+                }}
+                onFail={(err, name, newName) => { 
+                    dispatch(HideDialog());
+                    dispatch(ShowNotificationDialog(
+                        `Rename Entity ${name}`, 
+                        `Fail renaming entity ${name} into ${newName}, error: ${err}`
+                    ));
+                }}
+            />
+            <DialogNotification 
+                title={notification ? notification.title : ''} 
+                content={notification ? notification.content : ''}
+                dialogProps={{
+                    open: Boolean(notification),
+                    onClose: ()=>dispatch(HideNotificationDialog())
+                }}
+            />
+        </React.Fragment>
+    )
+}
 
 const columns = [
     {
@@ -53,6 +147,7 @@ const columns = [
 // fetch authStatus from redux store and setAuthorized(true) if creator or manager
 // Any actions in Project will affect Entity and need Creator or Manager level Authorization
 function Project(props){
+    const dispatch = useDispatch();
     const {project} = props.match.params;
     const status = useSelector(state=>state.authStatus);
     const database = useSelector(
@@ -88,43 +183,57 @@ function Project(props){
     // how many items
 
     return(
-        <MaterialTable 
-            title="Entities" 
-            columns={columns} 
-            data={datas}
-            actions={authorized ? [
-                {
-                    icon: ()=><Create/>, 
-                    tooltip: "Rename Entity", 
-                    onClick: ()=>{}
-                },
-                {
-                    icon: ()=><DeleteForever/>, 
-                    tooltip: "Delete Entity",
-                    onClick: ()=>{}
-                },
-                {
-                    icon: ()=><Add/>,
-                    tooltip: "Add Entity",
-                    isFreeAction: true,
-                    onClick: ()=>{}
-                }
-            ]: []}
-            icons={{
-                Search: Search,
-                ResetSearch: Clear,
-                FirstPage: FirstPage,
-                PreviousPage: ArrowBack,
-                NextPage: ArrowForward,
-                LastPage: LastPage
-            }}
-            options={{
-                pageSize: 10,
-                pageSizeOptions: [10,25,50],
-                search: true,
-                actionsColumnIndex: -1
-            }}
-        />
+        <React.Fragment>
+            {authorized ? <Authorized/> : null}
+            <MaterialTable 
+                title="Entities" 
+                columns={columns} 
+                data={datas}
+                actions={authorized ? [
+                    {
+                        icon: ()=><Create/>, 
+                        tooltip: "Rename Entity", 
+                        onClick: (e, rowData)=>{
+                            const {id, name} = rowData;
+                            dispatch(SetTarget(id, name));
+                            dispatch(ShowRenameDialog());
+                        }
+                    },
+                    {
+                        icon: ()=><DeleteForever/>, 
+                        tooltip: "Delete Entity",
+                        onClick: (e, rowData)=>{
+                            const {id, name} = rowData;
+                            dispatch(SetTarget(id, name));
+                            dispatch(ShowDeleteDialog());
+                        }
+                    },
+                    {
+                        icon: ()=><Add/>,
+                        tooltip: "Add Entity",
+                        isFreeAction: true,
+                        onClick: (e)=>{
+                            dispatch(SetTarget(database.id, ""));
+                            dispatch(ShowAddDialog());
+                        }
+                    }
+                ]: []}
+                icons={{
+                    Search: Search,
+                    ResetSearch: Clear,
+                    FirstPage: FirstPage,
+                    PreviousPage: ArrowBack,
+                    NextPage: ArrowForward,
+                    LastPage: LastPage
+                }}
+                options={{
+                    pageSize: 10,
+                    pageSizeOptions: [10,25,50],
+                    search: true,
+                    actionsColumnIndex: -1
+                }}
+            />
+        </React.Fragment>
     )
 }
 
