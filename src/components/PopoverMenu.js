8a850f6d4dce09660ed27593a3bac61a3dc5a06c
Starfall Production <starfallproduction42@gmail.com>
2019-07-01 18:17:22 +0700

    Create component to handle PopoverMenu

---
+++ src/components/PopoverMenu.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import {Menu, MenuItem, ListItemIcon, Typography} from '@material-ui/core';
+import PropTypes from 'prop-types';
+
+function Icon({IconComponent}){
+    if(!IconComponent) return null;
+    return(
+        <ListItemIcon>
+            <IconComponent/>
+        </ListItemIcon>
+    )
+}
+
+// menus is list consist of
+// {title: string, onClick: fn, icon: component}
+function PopoverMenu(props){
+    const {menus} = props;
+
+    return(
+        <Menu {...props}>
+        {
+            menus.map((value, index)=>{
+                const title = value.title ? value.title : '';
+                const onClick = value.onClick ? value.onClick : ()=>{};
+
+                return(
+                    <MenuItem key={index} onClick={onClick}>
+                        <Icon IconComponent={value.icon}/>
+                        <Typography>{title}</Typography>
+                    </MenuItem>
+                )
+            })
+        }
+        </Menu>
+    )
+}
+
+PopoverMenu.propTypes = {
+    menus: PropTypes.arrayOf(
+        PropTypes.shape({
+            title: PropTypes.string.isRequired,
+            icon: PropTypes.node,
+            onClick: PropTypes.func.isRequired
+        }).isRequired
+    ).isRequired,
+    ...Menu.propTypes
+}
+
+export default PopoverMenu;
