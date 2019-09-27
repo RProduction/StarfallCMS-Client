aceae2b6f4b6ff1619c2107ca07569d74f647a2d
Starfall Production <starfallproduction42@gmail.com>
2019-09-27 12:48:22 +0700

    Change OverviewCard code

--- src/components/OverviewCard.js
+++ src/components/OverviewCard.js
@@ -7,32 +7,32 @@ import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardMedia from '@material-ui/core/CardMedia';
-import List from '@material-ui/core/List';
 import Avatar from '@material-ui/core/Avatar';
-
-import OverviewList from './OverviewList';
+import TextField from '@material-ui/core/TextField';
+import makeStyles from '@material-ui/core/styles/makeStyles';
 
 const OverviewCardAction = lazy(() => import('./OverviewCardAction'));
 
 const Index = ({index}) => <Avatar>{index}</Avatar>;
 const Title = ({name}) => <Typography variant="h4" style={{ color: "black" }}>{name}</Typography>
 
-function Detail({primary}){
-    return(
-        <OverviewList listItemProp={{dense: true}}
-            listItemTextProp={{
-                primaryTypographyProps:{variant: 'body2'},
-                primary: primary
-            }}
-        />
-    )
-}
+const useStyle = makeStyles((theme) => ({
+    root: {
+        paddingBottom: theme.spacing(0.5),
+        paddingTop: theme.spacing(0.5)
+    },
+    horizontal: {
+        paddingLeft: theme.spacing(0.5),
+        paddingRight: theme.spacing(0.5)
+    }
+}));
 
 // Overview Card actions will only available for creator
 // actions consist of renaming and deleting Project
 // and those action need Creator Authorization
 function OverviewCard(props){
     const {name, index, id, img, updated, created, publicKey, authorized} = props;
+    const style = useStyle();
     
     return(
         <Grid item container xs={12} justify="center">
@@ -48,10 +48,31 @@ function OverviewCard(props){
                 <Box component={CardMedia} height={250} title={name}
                     image={img}
                 />
-                <CardContent component={List}>
-                    <Detail primary={`Public Key: ${publicKey}`}/>
-                    <Detail primary={`Created At: ${created}`}/>
-                    <Detail primary={`Updated At: ${updated}`}/>
+                <CardContent component={Grid} container>
+                    <Grid xs={12} container item 
+                        alignItems="center" className={style.root}
+                    >
+                        <Grid component={Typography} item xs={"auto"}>
+                            Public Key:
+                        </Grid>
+                        <Grid component={TextField} item xs className={style.horizontal}
+                            value={publicKey} variant="outlined"
+                            inputProps={{
+                                onClick: (e) => {
+                                    e.target.select();
+                                    e.target.setSelectionRange(0, 99999);
+                                    document.execCommand("copy");
+                                    alert("Copied Public Key");
+                                }
+                            }}
+                        />
+                    </Grid>
+                    <Grid xs={12} item component={Typography} className={style.root}>
+                        {`Created At: ${created}`}
+                    </Grid>
+                    <Grid xs={12} item component={Typography} className={style.root}>
+                        {`Updated At: ${updated}`}
+                    </Grid>
                 </CardContent>
             </Box>
         </Grid>
